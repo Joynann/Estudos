800428de96ffd3433791c14f02176cbd1e759fed
Joynan <joynan112@gmail.com>
2021-11-10 16:29:07 -0300

    Atualizando exercícios Javascript
    Atualização de exercícios de Javascript do curso.

---
+++ exercicios origamid/Javascript/tudo-eh-objeto/script.js
@@ -0,0 +1,20 @@
+// nomeie 3 propriedades ou métodos de strings
+
+var string = 'string';
+// match
+// normalize
+// repeat
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+var btn = document.querySelector('.btn');
+// replaceChild
+// requestFullscreen
+// scrollIntoView
+// setAttribute
+// toggleAttribute
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// https://clipboardjs.com/
