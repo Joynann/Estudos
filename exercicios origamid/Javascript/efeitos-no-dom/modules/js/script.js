a9484d0b79fbc6394be9c40f3f78fda70b1a8c9a
Joynan <joynan112@gmail.com>
2021-12-11 10:43:19 -0300

    Exercícios JS
    Atualizando exercícios de JS

---
+++ exercicios origamid/Javascript/efeitos-no-dom/modules/js/script.js
@@ -0,0 +1,9 @@
+const html = document.documentElement.className += 'js';
+
+import initAnimaScroll from './modules/anima-scroll.js';
+import initNavTab from './modules/navegacao-tab.js';
+import initNavAccordion from './modules/navegacao-accordion.js';
+
+initAnimaScroll();
+initNavTab();
+initNavAccordion();
