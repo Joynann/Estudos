0264223b18b0b60268f03eda7a2179b219a1be29
Joynan <joynan112@gmail.com>
2021-12-15 09:32:20 -0300

    Organizando código
    Foi feito uma organização no código do arquivo tooltip.js

--- exercicios origamid/Javascript/efeitos-no-dom/tooltip/js/modules/tooltip.js
+++ exercicios origamid/Javascript/efeitos-no-dom/tooltip/js/modules/tooltip.js
@@ -1,41 +1,50 @@
-export default function initToolTip() {
+export default function initToolTip()
+{
+    const tooltips = document.querySelectorAll('[data-tooltip]');
 
-}
-
-const tooltips = document.querySelectorAll('[data-tooltip]');
-
-function onMouseOver() {
-    const tooltip = criarToolTip(this);
-    onMouseMove.tooltip = tooltip;
-    onMouseLeave.tooltip = tooltip;
-    onMouseLeave.element = this;
-    this.addEventListener('mousemove', onMouseMove);
-    this.addEventListener('mouseleave', onMouseLeave);
-}
-
-function criarToolTip(map) {
-    const tooltip = document.createElement('div');
-    tooltip.innerHTML = map.getAttribute('aria-label');
-    tooltip.classList.add('tooltip');
-    document.body.appendChild(tooltip);
-    return tooltip;
-}
-
-const onMouseMove = {
-    handleEvent(e) {
-        this.tooltip.style.top = e.pageY + 20 + 'px';
-        this.tooltip.style.left = e.pageX + 20 + 'px';
+    if(tooltips)
+    {
+        function onMouseOver()
+        {
+            const tooltip = criarToolTip(this);
+            onMouseMove.tooltip = tooltip;
+            onMouseLeave.tooltip = tooltip;
+            onMouseLeave.element = this;
+            this.addEventListener('mousemove', onMouseMove);
+            this.addEventListener('mouseleave', onMouseLeave);
+        }
+        
+        function criarToolTip(map)
+        {
+            const tooltip = document.createElement('div');
+            tooltip.innerHTML = map.getAttribute('aria-label');
+            tooltip.classList.add('tooltip');
+            document.body.appendChild(tooltip);
+            return tooltip;
+        }
+        
+        const onMouseMove =
+        {
+            handleEvent(e)
+            {
+                this.tooltip.style.top = e.pageY + 20 + 'px';
+                this.tooltip.style.left = e.pageX + 20 + 'px';
+            }
+        }
+        
+        const onMouseLeave =
+        {
+            handleEvent()
+            {
+                document.body.removeChild(this.tooltip);
+                this.element.removeEventListener('mouseleave', onMouseLeave);
+                this.element.removeEventListener('mousemove', onMouseMove);
+            }
+        }
+        
+        tooltips.forEach((i) =>
+        {
+            i.addEventListener('mouseover', onMouseOver);
+        });
     }
 }
-
-const onMouseLeave = {
-    handleEvent() {
-        document.body.removeChild(this.tooltip);
-        this.element.removeEventListener('mouseleave', onMouseLeave);
-        this.element.removeEventListener('mousemove', onMouseMove);
-    }
-}
-
-tooltips.forEach((i) => {
-    i.addEventListener('mouseover', onMouseOver);
-});
