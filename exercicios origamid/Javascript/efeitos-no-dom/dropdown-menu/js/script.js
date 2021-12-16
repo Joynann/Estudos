e27b6a5a51887f327a4247de64c007e0a44f3765
Joynan <joynan112@gmail.com>
2021-12-16 14:25:28 -0300

    Exercício JS
    Atualizando exercícios de JS

---
+++ exercicios origamid/Javascript/efeitos-no-dom/dropdown-menu/js/script.js
@@ -0,0 +1,15 @@
+const html = document.documentElement.className += 'js';
+
+import initAnimaScroll from './modules/anima-scroll.js';
+import initNavTab from './modules/navegacao-tab.js';
+import initNavAccordion from './modules/navegacao-accordion.js';
+import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropdown-menu.js';
+
+initAnimaScroll();
+initNavTab();
+initNavAccordion();
+initModal();
+initToolTip();
+initDropDownMenu();
