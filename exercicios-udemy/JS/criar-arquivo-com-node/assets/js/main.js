bd1e553e7ce8060fd1d565b91cf0871f09005374
Joynan <joynan112@gmail.com>
2022-04-14 10:03:24 -0300

    Ler arquivo com node
    Adicionando função de ler arquivo com node na pasta de criar arquivo.

--- exercicios-udemy/JS/criar-arquivo-com-node/assets/js/main.js
+++ exercicios-udemy/JS/criar-arquivo-com-node/assets/js/main.js
@@ -1,13 +1,19 @@
 const path = require('path');
-const criaArquivo = require('./modules/criaArquivo')
+const criaArquivo = require('./modules/criaArquivo');
+const leArquivo = require('./modules/leArquivo');
 
 const obj = 
 [{
     nome: 'Joynan',
     sobrenome: 'Lagass Loose',
     idade: 22,
+    aniversario: '11/05/1999',
 }];
 
 const jsonObj = JSON.stringify(obj, '', 2);
 
-criaArquivo(path.resolve(__dirname, '..', 'arquivos'), 'teste', 'json', `,\n${jsonObj}`, 'a');
+criaArquivo(path.resolve(__dirname, '..', 'arquivos'), 'teste', 'json', jsonObj);
+
+const caminho = path.resolve(__dirname, '..', 'arquivos', 'teste.json');
+
+leArquivo(caminho);
