23683562f8233307319c7a3e14c8c1437f17dd6f
Joynan <joynan112@gmail.com>
2022-04-17 15:29:24 -0300

    Modelo de configuração
    Adicionando modelo de configuração, Express + Webpack, entre outros recursos.

---
+++ exercicios-udemy/JS/express/express-webpack/routes.js
@@ -0,0 +1,13 @@
+// Imports
+const express = require('express');
+const router = express.Router();
+
+// Imports controllers
+const home = require('./src/controllers/home');
+
+// Response
+// home
+router.get('/', home.home);
+
+// Export Router
+exports.router = router;
