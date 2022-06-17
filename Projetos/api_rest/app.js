aae88f0562c3020ffa7f665ca81cba048f649576
Joynan <joynan112@gmail.com>
2022-06-17 15:52:45 -0300

    Verificando com Token
    Verificando autenticidade de usuário com Token por Middleware.

--- Projetos/api_rest/app.js
+++ Projetos/api_rest/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import homeRoute from './src/routes/homeRoute';
 import userRoute from './src/routes/userRoute';
 import tokenRoute from './src/routes/tokenRoute';
+import loginRequired from './src/middlewares/loginRequired';
 
 dotenv.config();
 
@@ -19,7 +20,7 @@ class App {
   }
 
   routes() {
-    this.app.use('/', homeRoute);
+    this.app.use('/', loginRequired, homeRoute);
     this.app.use('/users/', userRoute);
     this.app.use('/tokens/', tokenRoute);
   }
