83f0ff500245ab628497533ced8c7e840f5cb356
Joynan <joynan112@gmail.com>
2022-06-28 16:45:33 -0300

    CRUD de alunos
    Fazendo um CRUD completo de alunos.

--- Projetos/api_rest/src/controllers/AlunoController.js
+++ Projetos/api_rest/src/controllers/AlunoController.js
@@ -5,6 +5,67 @@ class HomeController {
     const aluno = await Aluno.findAll();
     res.json(aluno);
   }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ errors: 'Faltando id.' });
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) return res.status(400).json({ errors: 'Aluno não existe.' });
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ errors: 'Faltando id.' });
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) return res.status(400).json({ errors: 'Aluno não existe.' });
+
+      const novoAluno = await aluno.update(req.body);
+
+      return res.json(novoAluno);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ errors: 'Faltando id.' });
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) return res.status(400).json({ errors: 'Aluno não existe.' });
+
+      aluno.destroy();
+
+      return res.json({ success: 'Apagado com sucesso.' });
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
 }
 
 export default new HomeController();
