e9adc3e0245a15991fc825e2df32f42b6902d599
Joynan <joynan112@gmail.com>
2022-06-30 15:27:57 -0300

    Tratando arquivo
    Tratando arquivo para que só aceite png e jpeg.

--- Projetos/api_rest/src/controllers/FotoController.js
+++ Projetos/api_rest/src/controllers/FotoController.js
@@ -1,8 +1,19 @@
-// import Aluno from '../models/Aluno';
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
 
 class FotoController {
-  async index(req, res) {
-    res.json(req.file);
+  async store(req, res) {
+    return upload(req, res, (error) => {
+      if (error) {
+        res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- Projetos/api_rest/src/routes/fotoRoute.js
+++ Projetos/api_rest/src/routes/fotoRoute.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
 
 const router = new Router();
-const upload = multer(multerConfig);
 
-router.post('/', upload.single('foto'), fotoController.index);
+router.post('/', fotoController.store);
 
 export default router;
