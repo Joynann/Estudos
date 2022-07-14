695f46aa6659e18ea0fcc26816c388932a11a649
Joynan <joynan112@gmail.com>
2022-07-14 13:12:45 -0300

    Adicionado opção de editar
    Adicionado método para editar tarefa.
    Adicionado botão para cancelar edição.

--- Projetos/listatarefa/src/components/Main.js
+++ Projetos/listatarefa/src/components/Main.js
@@ -6,17 +6,33 @@ export default class Main extends Component {
   state = {
     novaTarefa: '',
     tarefas: [],
+    index: -1,
+    classeAtivo: 'desativado',
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
-    const { novaTarefa } = this.state;
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
 
-    if (tarefas.indexOf(novaTarefa) !== -1) return;
+    if (tarefas.indexOf(novaTarefa) !== -1 || !novaTarefa) return;
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...tarefas, novaTarefa],
+      });
+    } else {
+      tarefas[index] = novaTarefa;
+      this.setState({
+        tarefas: [...tarefas],
+        index: -1,
+        classeAtivo: 'desativado',
+      });
+    }
 
     this.setState({
-      tarefas: [...tarefas, novaTarefa],
+      novaTarefa: '',
     });
   };
 
@@ -27,7 +43,22 @@ export default class Main extends Component {
   };
 
   handleEdit = (index) => {
-    console.log(index);
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+      classeAtivo: 'ativo',
+    });
+  };
+
+  handleNoEdit = (e) => {
+    e.preventDefault();
+    this.setState({
+      novaTarefa: '',
+      index: -1,
+      classeAtivo: 'desativado',
+    });
   };
 
   handleDelete = (index) => {
@@ -35,23 +66,37 @@ export default class Main extends Component {
     tarefas.splice(index, 1);
     this.setState({
       tarefas: [...tarefas],
+      novaTarefa: '',
+      index: -1,
+      classeAtivo: 'desativado',
     });
   };
 
   render() {
-    const { novaTarefa, tarefas } = this.state;
+    const { novaTarefa, tarefas, classeAtivo } = this.state;
 
     return (
       <div className="main">
 
         <h1>Lista de tarefas</h1>
         <form onSubmit={this.handleSubmit} action="#" className="form">
+          <button
+            onClick={this.handleNoEdit}
+            type="submit"
+            className={classeAtivo}
+          >
+            <FaWindowClose
+              onClick={this.handleNoEdit}
+              className="noEdit"
+            />
+          </button>
+
           <input
             type="text"
             onChange={this.handleChange}
             value={novaTarefa}
           />
-          <button type="submit">
+          <button type="submit" className="submit">
             <FaPlus />
           </button>
         </form>
