dfd7a28674d3cf854578fc9fa4c1fe84c6bd155c
Joynan <joynan112@gmail.com>
2022-08-04 14:06:54 -0300

    Configurando History
    Fazendo implementação do history na aplicação

--- Projetos/react/react-base/src/App.js
+++ Projetos/react/react-base/src/App.js
@@ -1,14 +1,16 @@
-import Login from './pages/Login';
+import { Router } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
+import Routes from './Routes';
+import history from './services/history';
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyle />
-    </>
+    </ Router>
   );
 }
 
