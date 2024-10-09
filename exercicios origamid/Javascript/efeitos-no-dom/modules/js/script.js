6e7c75505a6aa45066f310cf90e9b712c3d72be6
Joynan <joynan112@gmail.com>
2024-10-08 22:56:52 -0300

    Delete exercicios origamid directory
    Organizando arquivos no git.

--- exercicios origamid/Javascript/efeitos-no-dom/modules/js/script.js
+++
@@ -1,9 +0,0 @@
-const html = document.documentElement.className += 'js';
-
-import initAnimaScroll from './modules/anima-scroll.js';
-import initNavTab from './modules/navegacao-tab.js';
-import initNavAccordion from './modules/navegacao-accordion.js';
-
-initAnimaScroll();
-initNavTab();
-initNavAccordion();
