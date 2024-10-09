ca90da9f47e54a2f5f24137c7d8958ecab65f3bb
Joynan <joynan112@gmail.com>
2024-10-08 22:57:54 -0300

    Delete Projetos/api_rest directory
    Organizando arquivos no git.

--- Projetos/api_rest/server.js
+++
@@ -1,6 +0,0 @@
-import app from './app';
-
-const port = 3001;
-app.listen(port, () => {
-  console.log('\n', `Escutando na porta ${port}`, '\n', `CTRL + CLICK para acessar http://localhost:${port}`, '\n');
-});
